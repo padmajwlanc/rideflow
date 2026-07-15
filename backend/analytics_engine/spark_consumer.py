028bffbbdf8cd33fd8e2eee9e87e1d6dc2b477d9
Padmaj Hangargekar <padmajmahan@gmail.com>
2026-07-15 08:58:04 +0530

    Remove virtual environment and add gitignore

--- backend/analytics_engine/spark_consumer.py
+++ backend/analytics_engine/spark_consumer.py
@@ -4,9 +4,33 @@ spark = (
     SparkSession.builder
     .appName("RideFlow Analytics")
     .master("local[*]")
+    .config(
+        "spark.jars.packages",
+        "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0"
+    )
     .getOrCreate()
 )
 
-print("Spark Started Successfully!")
+spark.sparkContext.setLogLevel("ERROR")
 
-spark.stop()
+df = (
+    spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "ride_events")
+    .option("startingOffsets", "latest")
+    .load()
+)
+
+messages = df.selectExpr(
+    "CAST(value AS STRING)"
+)
+
+query = (
+    messages.writeStream
+    .outputMode("append")
+    .format("console")
+    .start()
+)
+
+query.awaitTermination()
